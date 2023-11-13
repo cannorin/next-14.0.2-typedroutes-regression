9b16194c1d1c808fc9d0f77604756b7684b19945
cannorin <cannorin@users.noreply.github.com>
2023-11-13 18:12:58 +0900

    Remove `const runtime = "edge"`

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,8 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// offending line
-export const runtime = 'edge'
-
 export async function GET(request: NextRequest) {
   return NextResponse.json({ hello: 'world' });
 }
